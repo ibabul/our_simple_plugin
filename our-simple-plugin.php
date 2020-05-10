fe9f6e569451913510bf2ad4a34839197f0035de
M Yakub Mizan <ym@orchestra.ltd>
2020-05-10 22:36:14 +0600

    woocommerce add to cart actions updated

--- our-simple-plugin.php
+++ our-simple-plugin.php
@@ -19,6 +19,19 @@
 class OUR_SIMPLE_PLUGIN {
 	public function __construct() {
 		add_action('init', array($this, 'init'), 10, 0);
+
+		//testing and familiar with actions
+		add_action( 'woocommerce_before_cart_table', array($this,'wpdesk_cart_free_shipping_text'), 10,0 );
+
+		add_action( 'woocommerce_before_cart', array($this,'wooco_cart'), 10,0 );
+
+		add_action( 'woocommerce_before_cart_table', array($this,'wooco_cart2'), 10,0 );
+
+		add_action( 'woocommerce_cart_is_empty', array($this,'wooco_cart3'), 10,0 );
+
+
+
+
 	}
 
 	public function init() {
@@ -27,13 +40,9 @@ class OUR_SIMPLE_PLUGIN {
 
 		add_filter('woocommerce_get_regular_price', array( $this, 'my_custom_price'), 99);
 		add_filter('woocommerce_get_price', array( $this, 'my_custom_price'), 99);
-
-
-
-  	
-		
 		
 	}
+
 
 	public function filtered_title($title, $id ) {
 
@@ -55,6 +64,23 @@ class OUR_SIMPLE_PLUGIN {
   	return $new_price;
  }
 
+
+
+public function wpdesk_cart_free_shipping_text() {
+	echo '<div class="woocommerce-info">Free Shipping available from $29!</div>';
+}
+
+public function wooco_cart() {
+	echo '<p class="woocommerce-info">I am a test action who used 	woocommerce_before_cart action</p>';
+}
+
+public function wooco_cart2() {
+	echo '<p class="woocommerce-info">I am a test action who used 	woocommerce_before_cart_table action</p>';
+}
+
+public function wooco_cart3() {
+ 	get_empty() 
+}
 }
 
 
