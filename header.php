7633bad7f965eba93d3e892bf574b9af78d99520
ibabul <wpimtiaz@gmail.com>
2020-05-11 05:12:03 +0600

    Added css and manupulate plugins style with it

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+/*
+*This template will be use to add header in any php page
+*
+*/
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
--- our-simple-plugin.php
+++ our-simple-plugin.php
@@ -2,9 +2,9 @@
 /**
  * Our Simple plugin
  *
- * @package 	ourSimplePlugin
- * @author  	MD Imtiaz
- * @copyright	2020 md imtiaz
+ * @package   ourSimplePlugin
+ * @author    MD Imtiaz
+ * @copyright 2020 md imtiaz
  *
  * @wordpress-plugin
  * Plugin Name: Our Simple plugin
@@ -14,87 +14,135 @@
  * Version: 1.0.0
  * Requires at least: 5.3.0
  * Tested up to: 5.4
- **/ 
+ **/
+
+ define( 'OUR_SIMPLE_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
+ define( 'OUR_SIMPLE_PLUGIN_VERSION', '1.0.0.' );
 
 class OUR_SIMPLE_PLUGIN {
 
 
+
+
+	/**
+	 * @return void
+	 */
 	public function __construct() {
-		
-
-		add_action('init', array($this, 'init'), 10, 0);
-
-		//testing and familiar with actions
-		add_action( 'woocommerce_before_cart_table', array($this,'wpdesk_cart_free_shipping_text'), 10,0 );
-
-		add_action( 'woocommerce_before_cart', array($this,'wooco_cart'), 10,0 );
-
-		add_action( 'woocommerce_before_cart_table', array($this,'wooco_cart2'), 10,0 );
-
-		add_action( 'woocommerce_cart_is_empty', array($this,'wooco_cart3'), 10,0 );
-		add_action( 'woocommerce_cart_totals_after_order_total', array($this,'wooco_cart4'), 10,0 );
-
-
-		add_action( 'WP_ENEQUEUE_SCRIPTS' , array($this,'my_style'), 10,0);
+		add_action( 'init', array( $this, 'init' ), 10, 0 );
 	}
 
 
-
+	/**
+	 * @return void
+	 */
 	public function init() {
-		add_filter('the_title', array($this, 'filtered_title'), 10, 2);
+		add_filter( 'the_title', array( $this, 'filtered_title' ), 10, 2 );
 
+		add_filter( 'woocommerce_get_regular_price', array( $this, 'my_custom_price' ), 99 );
+		add_filter( 'woocommerce_get_price', array( $this, 'my_custom_price' ), 99 );
 
-		add_filter('woocommerce_get_regular_price', array( $this, 'my_custom_price'), 99);
-		add_filter('woocommerce_get_price', array( $this, 'my_custom_price'), 99);
-		
+		// testing and familiar with actions.
+		add_action( 'woocommerce_before_cart_table', array( $this, 'wpdesk_cart_free_shipping_text' ), 10, 0 );
+
+		add_action( 'woocommerce_before_cart', array( $this, 'wooco_cart' ), 10, 0 );
+
+		add_action( 'woocommerce_before_cart_table', array( $this, 'wooco_cart2' ), 10, 0 );
+
+		add_action( 'woocommerce_cart_is_empty', array( $this, 'wooco_cart3' ), 10, 0 );
+		add_action( 'woocommerce_cart_totals_after_order_total', array( $this, 'wooco_cart4' ), 10, 0 );
+
+		add_filter( 'admin_footer_text', array( $this, 'footer_manupulation' ), 10, 1 );
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_plugin_styles' ), 10, 0 );
+
 	}
 
 
-	public function filtered_title($title, $id ) {
+	/** to target each product title of woocommerce with product id
+	 *
+	 * @param STRING  $title
+	 * @param INTEGER $id
+	 *
+	 * @return STRING
+	 */
+	public function filtered_title( $title, $id ) {
 
 		$product = wc_get_product( $id );
 
-		if ( !empty($product) && $product->get_id() ) {
+		if ( ! empty( $product ) && $product->get_id() ) {
 			return 'Product: ' . $title;
 		}
 
 		return $title;
 	}
 
+	/**
+	 * To manupulate original product price of woocommerce
+	 *
+	 * @param INTEGER $original_price
+	 *
+	 * @return MIXED
+	 */
+
 	public function my_custom_price( $original_price ) {
-  		global $post, $woocommerce;
+		global $post, $woocommerce;
 
-  		$new_price = $original_price * 2;
-
-  	
-  	return $new_price;
- }
-
-public function my_style() {
-	wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css', array(), filemtime(get_template_directory() . '/css/style.css'), false);
-}
+		$new_price = $original_price;
+		return $new_price;
+	}
+ 
 
 
-public function wpdesk_cart_free_shipping_text() {
-	echo '<div class="woocommerce-info">Free Shipping available from $29!</div>';
-}
+	/** adding stylesheet
+	 *
+	 * @return style
+	 */
+	public function enqueue_plugin_styles() {
+		wp_register_style( 'our-custom-plugin-style', OUR_SIMPLE_PLUGIN_URI . '/css/custom_style.css', array(), OUR_SIMPLE_PLUGIN_VERSION );
+		wp_enqueue_style( 'our-custom-plugin-style' );
+	}
 
-public function wooco_cart() {
-	echo '<p class="woocommerce-info">I am a test action who used 	woocommerce_before_cart action</p>';
-}
 
-public function wooco_cart2() {
-	echo '<p class="woocommerce-info">I am a test action as used 	woocommerce_before_cart_table action</p>';
-}
+	/** Adding   text
+	 *
+	 * @return STRING
+	 */
+	public function wpdesk_cart_free_shipping_text() {
+		echo '<div class="woocommerce-info">Free Shipping available from $29!</div>';
+	}
 
-public function wooco_cart3() {
- 	echo '<p class="woocommerce-info para-1">I am a test action as used 	woocommerce_before_cart_table action</p>';
-}
+	/** Adding a text before cart action
+	 *
+	 * @return string;
+	 */
+	public function wooco_cart() {
+		echo '<p class="woocommerce-info">I am a test action who used woocommerce_before_cart action</p>';
+	}
 
-public function wooco_cart4() {
- 	echo '<p href="../" class="para-1" >Ready to pay sir</p>';
-}
+	/** Adding text before cart action
+	 *
+	 * @return string
+	 */
+	public function wooco_cart2() {
+		echo '<p class="woocommerce-info para-2">I am a test action as used woocommerce_before_cart_table action</p>';
+	}
 
+
+	public function wooco_cart3() {
+		echo '<p class="woocommerce-info para-1">I am a test action as used woocommerce_before_cart_table action</p>';
+	}
+
+	public function wooco_cart4() {
+		echo '<p href="../" class="para-1" >Ready to pay</p>';
+	}
+
+
+	public function footer_manupulation( $string ) {
+		echo( 'test' );
+		die;
+		$string .= 'I am taking footer alng with me';
+		return $string;
+	}
 }
 
 
